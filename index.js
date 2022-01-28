228f13e2edd07e786c790c58d6f0a3c776232ff8
sayan112 <sayanmaitraj5@gmail.com>
2022-01-28 16:48:52 +0530

    bubbles add

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 window.addEventListener("load",()=>{
     const sounds = document.querySelectorAll(".sound");
      const pads= document.querySelectorAll(".pads div");
+     const visual =  document.querySelector(".visual");
+     const colors = [ " #ff6b6b",
+     "#ff9ff3",
+    " #54a0ff",
+    " #5fd11d",
+    " #00d2d3",
+     "#A3CB38",
+    " #EE5A24",
+    " #786fa6",
+    " #FEA47F",
+    " #f5cd79"
+    ];
+
 
 
  pads.forEach((pad,index)=>{
@@ -8,6 +21,18 @@ window.addEventListener("load",()=>{
      {
          sounds[index].currentTime=0;
          sounds[index].play();
+          createBubbles(index);
      });
  });
+
+
+// create bubles
+ const createBubbles = (index)=>{
+      const bubble = document.createElement("div");
+       visual.appendChild(bubble);
+        bubble.style.backgroundColor= colors[index];
+         bubble.style.animation = "jump 1s ease";
+ };
+
+
 });
